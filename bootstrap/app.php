e89432168dff4806b76c307e030744bd3683b545
FelipeVegaEsparza <felipevegaesparza@gmail.com>
2026-09-21 15:53:26 -0300

    Confia en proxies para generar URLs https detras de Traefik

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsurePlatformAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,8 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Detrás de un proxy (Dokploy/Traefik) con HTTPS: confiar en las
+        // cabeceras X-Forwarded-* para generar URLs https y cookies seguras.
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-            'admin' => \App\Http\Middleware\EnsurePlatformAdmin::class,
+            'admin' => EnsurePlatformAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
